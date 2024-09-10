797c1a7efd98294b9afd7dcf414ebc0a2f5f20ab
AFZAAL-KHALID <muhammadafzaalkhalid2120353@gmail.com>
2024-09-10 04:19:50 -0800

    store and slice created by redux

---
+++ src/Store/Authslice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    status: false, //is  user auth or not
+    userData: null
+}
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        logIn:(state, action)=>{
+            state.status = true;
+            userData = action.payload.userData
+        },
+        logOut:(state)=>{
+            state.status = false;
+            state.userData = null
+        }
+
+    }
+})
+
+export const {logIn, logOut } = authSlice.actions; 
+export default authSlice.reducer
