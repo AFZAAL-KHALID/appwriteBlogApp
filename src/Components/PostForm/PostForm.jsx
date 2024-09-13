da00d68adb408a172db6749abc2b7cd0e66e5d05
AFZAAL-KHALID <muhammadafzaalkhalid2120353@gmail.com>
2024-09-12 20:41:15 -0800

    postForm Completed

--- src/Components/PostForm/PostForm.jsx
+++ src/Components/PostForm/PostForm.jsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { Container, SelectCompnt, Input, RTE } from "../index";
-import authservice from "../../appwriteServices/Auth_svc";
+import { SelectCompnt, Input, RTE } from "../index";
 import databaseSvcs from "../../appwriteServices/database_svc";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -62,7 +61,91 @@ const PostForm = (post) => {
     }
   };
 
-  return <div></div>;
+  //slug validation
+  const slugTransform = useCallback((value) => {
+    if (value && typeof value === "string") {
+      const slug = value.trim().toLowerCase().replace(/ /g, "-");
+      setValue("slug", slug);
+      return slug;
+    }
+    return "";
+  }, []);
+
+  useEffect(() => {
+    const subscription = watch((value, { name }) => {
+      if (name === "title") {
+        setValue("slug", slugTransform(value.title), { shouldValidate: true });
+      }
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [watch, slugTransform, setValue]);
+
+  return (
+    <form onSubmit={handleSubmit(submitFunction)} className="flex flex-wrap">
+      <div className="w-2/3 px-2">
+        <Input
+          label="Title :"
+          placeholder="Title"
+          className="mb-4"
+          {...register("title", { required: true })}
+        />
+        <Input
+          label="Slug :"
+          placeholder="Slug"
+          className="mb-4"
+          {...register("slug", { required: true })}
+          onInput={(e) => {
+            setValue("slug", slugTransform(e.currentTarget.value), {
+              shouldValidate: true,
+            });
+          }}
+        />
+        <RTE
+          label="Content :"
+          name="content"
+          control={control}
+          defaultValue={getValues("content")}
+        />
+      </div>
+
+      {/*right side ➡️ -------->>>           */}
+      <div className="w-1/3 px-2">
+        <Input
+          label="Featured Image :"
+          type="file"
+          className="mb-4"
+          accept="image/png, image/jpg, image/jpeg, image/gif"
+          {...register("image", { required: !post })}
+        />
+        {post && (
+          <div className="w-full mb-4">
+            <img
+              src={appwriteService.getFilePreview(post.featuredImage)}
+              alt={post.title}
+              className="rounded-lg"
+            />
+          </div>
+        )}
+        <SelectCompnt
+          options={["active", "inactive"]}
+          label="Status"
+          className="mb-4"
+          {...register("status", { required: true })}
+        />
+        //FIXME:⤵️
+        <Button
+          type="submit"
+          bgColor={post ? "bg-green-500" : undefined}
+          className="w-full"
+        >
+          {post ? "Update" : "Submit"}
+        </Button>
+      </div>
+    </form>
+  );
 };
 
 export default PostForm;
